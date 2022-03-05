db6133b062529a9a2b9685063c34b7a95cc85fd5
Rafael Conrado <rafael070893@hotmail.com>
2022-03-05 00:11:55 -0300

    end point update

--- src/routes/personsRoutes.js
+++ src/routes/personsRoutes.js
@@ -8,7 +8,8 @@ router.post("/", async (req, res) => {
     const { name, salary, approved } = req.body;
 
     if (!name || !salary || !approved) {
-        res.status(422).json({ erro: "Todos os campos são obrigatórios!" })
+        res.status(422).json({ erro: "Todos os campos são obrigatórios!" });
+        return;
     }
     const person = { name, salary, approved };
     try {
@@ -35,12 +36,49 @@ router.get('/', async (req, res) => {
 //Consultar usuário
 router.get('/:id', async (req, res) => {
     const id = req.params.id
+
+
     try {
         const person = await Person.findOne({ _id: id });
+
+        if (!person) {
+            res.status(422).json({ message: "Nenhum usuário encontrado!" });
+            return;
+        }
+
         res.status(200).json(person);
     } catch (error) {
         res.status(500).json({ error: error })
     }
+})
+
+//Atualizar dados
+
+router.patch('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const { name, salary, approved } = req.body;
+
+    const person = {
+        name,
+        salary,
+        approved
+    }
+    try {
+
+        const updatedPerson = await Person.updateOne({ _id: id }, person);
+
+        if (updatedPerson.matchedCount === 0) {
+            res.status(422).json({message: "Erro ao atualizar!"})
+        }
+
+        res.status(200).json(person)
+
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
 })
 
 module.exports = router;
