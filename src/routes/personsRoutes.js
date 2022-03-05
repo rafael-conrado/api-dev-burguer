d488e3ddfc9123968518d0633905b3ddff9b108f
Rafael Conrado <rafael070893@hotmail.com>
2022-03-04 23:40:57 -0300

    end point consultar todos e consultar um

--- src/routes/personsRoutes.js
+++ src/routes/personsRoutes.js
@@ -2,13 +2,13 @@ const Person = require("../models/Person");
 const router = require('express').Router();
 
 
-
+//criar usuário
 router.post("/", async (req, res) => {
     //req body
     const { name, salary, approved } = req.body;
 
     if (!name || !salary || !approved) {
-        res.status(422).json({erro:"Todos os campos são obrigatórios!"})
+        res.status(422).json({ erro: "Todos os campos são obrigatórios!" })
     }
     const person = { name, salary, approved };
     try {
@@ -21,5 +21,26 @@ router.post("/", async (req, res) => {
     }
 })
 
+//Consultar todos os usuários
+router.get('/', async (req, res) => {
+    try {
+        const people = await Person.find();
+
+        res.status(200).json(people);
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
+
+//Consultar usuário
+router.get('/:id', async (req, res) => {
+    const id = req.params.id
+    try {
+        const person = await Person.findOne({ _id: id });
+        res.status(200).json(person);
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+})
 
 module.exports = router;
